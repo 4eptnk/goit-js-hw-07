a80161c9a32d7f87729deae47dd20804735e67f5
Sergey Mychailichenko <4eptnk@gmail.com>
2021-04-29 23:50:22 +0300

    *
    *

--- source/Js/task-01.js
+++ source/Js/task-01.js
@@ -1,10 +1,16 @@
 const categoriesEl = document.getElementById('categories');
 console.log(`В списке ${categoriesEl.children.length} категории.`);
-for (let i = 0; i < categoriesEl.children.length; i++) {
-    const el = categoriesEl.children[i];
+var children = [...categoriesEl.children];
+children.forEach(el => {
     const h2 = el.getElementsByTagName('h2');
     console.log(`Категория: ${h2[0].innerText}`);
     console.log(`Количество элементов: ${el.getElementsByTagName('li').length}`);
-}
+});
+//for (let i = 0; i < categoriesEl.children.length; i++) {
+//    const el = categoriesEl.children[i];
+//    const h2 = el.getElementsByTagName('h2');
+//    console.log(`Категория: ${h2[0].innerText}`);
+//    console.log(`Количество элементов: ${el.getElementsByTagName('li').length}`);
+//}
 
 
