7d2a6bde4cc35e1a36b1087a25549ca38684613c
Sergey Mychailichenko <4eptnk@gmail.com>
2021-04-29 23:20:31 +0300

    *
    *

--- source/Js/task-05.js
+++ source/Js/task-05.js
@@ -1,7 +1,8 @@
 const input = document.getElementById('name-input')
 input.addEventListener('input', function(e) {
     var span = document.getElementById('name-output')
-    if (span.textContent = e.target.value) {
+    if (e.target.value.length > 0) {
+        span.textContent = e.target.value;
     } else {
         span.textContent = 'незнакомец';
     };
