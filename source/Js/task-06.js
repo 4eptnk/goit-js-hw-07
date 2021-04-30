8d12389564f11d3bcd1f5c0c4766f38a2754ec3a
Sergey Mychailichenko <4eptnk@gmail.com>
2021-04-30 23:50:18 +0300

    *
    *

--- source/Js/task-06.js
+++ source/Js/task-06.js
@@ -2,9 +2,11 @@ const input = document.getElementById('validation-input');
 
 function validate(e) {
     e.currentTarget.className = '';
-    if (e.currentTarget.value.length != e.currentTarget.dataset.length) {
-        return e.currentTarget.className = 'invalid';
+    if (e.currentTarget.value.length === parseInt(e.currentTarget.dataset.length)) {
+        return e.currentTarget.className = 'valid';
     }
-    return e.currentTarget.className = 'valid';
+    return e.currentTarget.className = 'invalid';
 }
 input.addEventListener('blur', validate)
+
+
